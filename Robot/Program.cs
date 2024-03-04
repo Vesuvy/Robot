665ee4b7d55eb77f10e1ade12415e4605ff4a7ba
vesuvy <baenkovav@mer.ci.nsu.ru>
2024-03-04 23:30:52 +0700

    Добавьте файлы проекта.

---
+++ Robot/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading;
+using RobotLibrary;
+
+namespace Robot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            RobotClass robot1 = new RobotClass("Робот 1", 10, 10);
+            RobotClass robot2 = new RobotClass("Робот 2", 0, 0);
+
+            for (int i = 0; i < 8; i++)
+            {
+                Thread moving1 = new Thread(robot1.Moving);
+                Thread moving2 = new Thread(robot2.Moving);
+
+                moving1.Start();
+                Thread.Sleep(20); // Минипауза во избежание одинакового выбора направлений
+                moving2.Start();
+
+                Thread.Sleep(2000); // Пауза между итерациями, чтобы увидеть результаты
+
+                moving1.Abort(); // Остановка потока перед созданием нового для следующей итерации
+                moving2.Abort();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ RobotLibrary/RobotClass.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Threading;
+
+namespace RobotLibrary
+{
+    public class RobotClass
+    {
+        private string Name { get; set; }
+        private int X { get; set; }
+        private int Y { get; set; }
+        public RobotClass(string name, int x, int y) { 
+            Name = name;
+            X = x;
+            Y = y;
+        }
+
+        public void Moving()
+        {
+
+            Random rnd = new Random();
+
+            int direction = 0;
+            string[] directions = { "вперед", "назад", "вправо", "влево" };
+
+            direction = rnd.Next(0, 4);
+            switch (direction)
+            {
+                case 0:
+                    Y++;
+                    Console.WriteLine($"{Name} движется {directions[direction]} -> (X,Y) - ({X},{Y})");
+                    break;
+                case 1:
+                    Y--;
+                    Console.WriteLine($"{Name} движется {directions[direction]} -> (X,Y) - ({X},{Y})");
+                    break;
+                case 2:
+                    X++;
+                    Console.WriteLine($"{Name} движется {directions[direction]} -> (X,Y) - ({X},{Y})");
+                    break;
+                case 3:
+                    X--;
+                    Console.WriteLine($"{Name}  движется {directions[direction]} -> (X,Y) - ({X},{Y})");
+                    break;
+            }
+            Thread.Sleep(550);
+        }
+    }        
+}
